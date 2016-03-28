1ef80b613025dd5c3b2dab89e8f798f7da9b1d7b
jtan25 <jhnnytny@gmail.com>
2016-03-28 04:40:49 +0000

    More init files

---
+++ proj/misc/LexRank.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from sumy.nlp.stemmers import Stemmer
+from sumy.parsers.plaintext import PlaintextParser
+
+#My own implementation of LexRank using sumy libaries
+
+
+def summarize(text, lines):
+    #parser =  PlaintextParser.from_file()
+    pass
---
+++ proj/parser/rawtext.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+
+from sumy._compat import to_unicode
+from sumy.utils import cached_property
+from sumy.models.dom import Sentence, Paragraph, ObjectDocumentModel
+from sumy.parsers.parser import DocumentParser
+
+
+
+#The built in plaintext parser takes in a format where there are headings (all uppercase) to denoted separation of paragraphs in text
+
+
+
+class RawTextParser(DocumentParser):
+
+        
+    @classmethod
+    def from_string(cls, string, tokenizer):
+        return cls(string, tokenizer)
+        
+        
+    @classmethod
+    def from_file(cls, file_path, tokenizer):
+        with open(file_path) as file:
+            return cls(file.read(), tokenizer)
+            
+    def __init__(self, text, tokenizer):
+        super(RawTextParser, self).__init__(tokenizer)
+        self._text = to_unicode(text).strip()
