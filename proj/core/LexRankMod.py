d18c7292be1b3e74c78098bb112438e8abbd9b24
jtan25 <jhnnytny@gmail.com>
2016-03-28 04:14:53 +0000

    call method added

--- proj/core/LexRankMod.py
+++ proj/core/LexRankMod.py
@@ -18,5 +18,20 @@ class LexRankModSummarizer(AbstractSummarizer):
     threshold = 0.1
     epsilon = 0.1
     _stop_words = frozenset()
-    pass
+
+    @property
+    def stop_words(self):
+        return self._stop_words
+    
+    @stop_words.setter
+    def stop_words(self,words):
+        self._stop_words = frozenset(map(self.normalize_word, words))
+    
+    #Overwritten call method to take a query
+    def __call__(self, document, sentences_count, query):
+        pass
+    
+    
+    
+    
 
